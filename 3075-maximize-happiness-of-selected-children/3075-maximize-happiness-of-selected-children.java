ca019754befe9bca74b7f64d7e52e450524f1ba2
Suryagayathri K P <128454366+suryagayathri01@users.noreply.github.com>
2024-10-04 22:16:28 +0530

    Time: 34 ms (91.99%), Space: 61.4 MB (79.67%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -4,19 +4,13 @@ class Solution {
         int n=happiness.length;
         Arrays.sort(happiness);
         for(int i=0;i<k;i++){
-            //num=Arrays.stream(happiness).max().getAsInt();
             num=happiness[n-i-1];
-            System.out.println(num);
-            if (num>=0){
-                if(num-i < 0){
-                    res+=0;
-                }else{
-                res+=num-i;
-                }
+            if(num-i < 0){
+                res+=0;
+            }else{
+            res+=num-i;
             }
         }
         return res;
-
-        
     }
 }
