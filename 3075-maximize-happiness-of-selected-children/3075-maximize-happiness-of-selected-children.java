3d192c7fa049b4adb29325ba1f7d1a2435790228
Suryagayathri K P <128454366+suryagayathri01@users.noreply.github.com>
2024-10-04 21:55:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        long res=0,num;
+        int n=happiness.length;
+        Arrays.sort(happiness);
+        for(int i=0;i<k;i++){
+            //num=Arrays.stream(happiness).max().getAsInt();
+            num=happiness[n-i-1];
+            System.out.println(num);
+            if (num>=0){
+                res+=num-i;
+            }
+        }
+        return res;
+
+        
+    }
+}
