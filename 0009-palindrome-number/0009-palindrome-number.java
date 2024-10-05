b57606ee4021e3a5099a0482d91b1133c8c098fd
Suryagayathri K P <128454366+suryagayathri01@users.noreply.github.com>
2024-10-05 19:25:18 +0530

    Time: 6 ms (30.84%), Space: 43.7 MB (77.87%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -5,11 +5,7 @@ class Solution {
         }
         int z=x;
         int len=String.valueOf(x).length();
-        if(len==2){
-            if(x/10 == x%10){
-                return true;
-            }
-        }
+        
         int y=0;
         for(int i=0;i<len;i++){
             int flag=x%10;
