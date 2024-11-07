ea224b663f20aa1f30a2f682ad05c7daddd36e49
Suryagayathri K P <128454366+suryagayathri01@users.noreply.github.com>
2024-11-07 21:15:30 +0530

    Time: 32 ms (74.6%), Space: 56.1 MB (87.65%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.java
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.java
@@ -1,10 +1,9 @@
 class Solution {
     public int largestCombination(int[] candidates) {
         int[] countbit=new int[24];
-        for(int i:candidates){
-            String s=String.format("%24s",Integer.toBinaryString(i)).replace(' ','0');
-            for(int j=0;j<24;j++){
-                if(s.charAt(j)=='1') countbit[j]++;
+        for(int j=0;j<24;j++){
+            for(int i:candidates){
+                if((i & (1<<j))>0) countbit[j]++;
             }
         }
         int big=0;
