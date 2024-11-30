b2d35382c54753c75f917b5a3a032518a77cac80
Suryagayathri K P <128454366+suryagayathri01@users.noreply.github.com>
2024-11-30 15:52:46 +0530

    Time: 8 ms (14.31%), Space: 46.9 MB (8.26%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans=new int[n+1];
+        for(int i=0;i<=n;i++){
+            String temp=Integer.toBinaryString(i);
+            int count=0;
+            for(char c:temp.toCharArray()){
+                if(c=='1') count++;
+            }
+            ans[i]=count;
+        }
+        return ans;
+    }
+}
